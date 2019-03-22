11ed1a57218bfa2ce50f3a604d9977651b59ef5f
DanielSolomonov <fsolomonov@team.amocrm.com>
2019-03-22 16:03:37 +0300

    good

--- app/Card.js
+++ app/Card.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import CheckList from './CheckList';
 class Card extends Component {
-  constructor() {
+    constructor() {
         super(...arguments);
         this.state = {
             showDetails: false
         };
     }
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
     render() {
-      let cardDetails;
-      if (this.state.showDetails) {
-          cardDetails = (
-              <div className="card__details">
-                  {this.props.description}
-                  <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-              </div>
-          );
-      }
-      return (
-        <div className="card" onClick={()=>this.setState({showDetails: !this.state.showDetails})}>{this.props.title}>
-            <div className="card__title">{this.props.title}</div>
-            {cardDetails}
-        </div>
-    );
+        let cardDetails;
+        if (this.state.showDetails) {
+            cardDetails = (
+                <div className="card__details">
+                    {this.props.description}
+                    <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+                </div>
+            );
+        }
+        return (
+            <div className="card">
+                <div
+                    className={
+                        this.state.showDetails ? "card__title card__title--is-open" : "card__title"
+                    }
+                    onClick={this.toggleDetails.bind(this)}>
+                    {this.props.title}
+                </div>
+                {cardDetails}
+            </div>
+        );
     }
 }
 export default Card;
